572f0a204d948acfbe990a766bb7cb1121cb2a17
Juan Yate Méndez <juanmanuelyatemendez@gmail.com>
2021-03-20 07:54:42 -0500

    Inicializando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'productos', component: ProductosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { RouterModule } from '@angular/router';
+import { RegistroComponent } from './components/registro/registro.component';
+import { IndexComponent } from './components/index/index.component';
+import { InfoComponent } from './components/info/info.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CarouselComponent,
+    LoginComponent,
+    FooterComponent,
+    NavbarComponent,
+    CardsComponent,
+    ProductosComponent,
+    RegistroComponent,
+    IndexComponent,
+    InfoComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
