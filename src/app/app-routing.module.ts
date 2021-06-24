416c893b8017088bf9e8746e761409aa006061e2
manueljuan123 <jmyate5@misena.edu.co>
2021-06-24 17:26:58 -0500

    Eliminación de la opción comprar y productos
    Se eliminaron los contenedores de comprar y de productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContenedorHomeComponent } from './components-contenedores/contenedor-home/contenedor-home.component';
 import { ContenedorRegistroComponent } from './components-contenedores/contenedor-registro/contenedor-registro.component';
-import { ContenedorProductosComponent } from './components-contenedores/contenedor-productos/contenedor-productos.component';
 import { ContenedorCitasComponent } from './components-contenedores/contenedor-citas/contenedor-citas.component';
-import { ContenedorComprarComponent } from './components-contenedores/contenedor-comprar/contenedor-comprar.component';
 import { CitasComponent } from './components-contenedores/citas/citas.component';
 
 
@@ -12,8 +10,6 @@ const routes: Routes = [
   {path: '', component: ContenedorHomeComponent},
   {path: 'registro', component: ContenedorRegistroComponent},
   {path: 'citas', component: ContenedorCitasComponent},
-  {path: 'productos', component: ContenedorProductosComponent},
-  {path: 'comprar', component: ContenedorComprarComponent},
   {path: 'citas', component: CitasComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,12 @@ import { LoginComponent } from './components-contenedores/login/login.component'
 import { FooterComponent } from './components-contenedores/footer/footer.component';
 import { NavbarComponent } from './components-contenedores/navbar/navbar.component';
 import { CardsComponent } from './components-contenedores/cards/cards.component';
-import { ProductosComponent } from './components-contenedores/productos/productos.component';
 import { RouterModule } from '@angular/router';
 import { RegistroComponent } from './components-contenedores/registro/registro.component';
 import { InfoComponent } from './components-contenedores/info/info.component';
 import { ContenedorHomeComponent } from './components-contenedores/contenedor-home/contenedor-home.component';
 import { ContenedorRegistroComponent } from './components-contenedores/contenedor-registro/contenedor-registro.component';
-import { ContenedorProductosComponent } from './components-contenedores/contenedor-productos/contenedor-productos.component';
 import { ContenedorCitasComponent } from './components-contenedores/contenedor-citas/contenedor-citas.component';
-import { ContenedorComprarComponent } from './components-contenedores/contenedor-comprar/contenedor-comprar.component';
 import { CitasComponent } from './components-contenedores/citas/citas.component';
 import { FormularioComponent } from './components-contenedores/formulario/formulario.component';
 import { ImagenComponent } from './components-contenedores/imagen/imagen.component';
@@ -31,14 +28,11 @@ import { ImagenComponent } from './components-contenedores/imagen/imagen.compone
     FooterComponent,
     NavbarComponent,
     CardsComponent,
-    ProductosComponent,
     RegistroComponent,
     InfoComponent,
     ContenedorHomeComponent,
     ContenedorRegistroComponent,
-    ContenedorProductosComponent,
     ContenedorCitasComponent,
-    ContenedorComprarComponent,
     CitasComponent,
     FormularioComponent,
     ImagenComponent
